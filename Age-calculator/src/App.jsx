aace59bc08edeb09123545e72021030dca4a57d2
David Esteban Gallego <dgallego@grupoasd.com>
2024-02-29 09:59:11 -0500

    add comments

--- Age-calculator/src/App.jsx
+++ Age-calculator/src/App.jsx
@@ -2,7 +2,7 @@ import styles from './App.module.css';
 import { AgeCalculator } from './components/age-calculator/ageCalculator';
 
 function App() {
-  
+  //las clases no se nombran con mayusculas styles.App ------> styles.app
   return (
     <div className={styles.App}>
       <AgeCalculator />
--- Age-calculator/src/components/age-calculator/ageCalculator.jsx
+++ Age-calculator/src/components/age-calculator/ageCalculator.jsx
@@ -4,72 +4,80 @@ import data from './data.json'
 
 export const AgeCalculator = () => {
 
-    const [birthDate, setBirthDate] = useState({});
-    const [age, setAge] = useState({years: 0,months: 0, days: 0 });
+  const [birthDate, setBirthDate] = useState({});
+  const [age, setAge] = useState({ years: 0, months: 0, days: 0 });
 
-    const handleInputChange = (label) => (event) => {
-        setBirthDate({
-            ...birthDate,
-            [label]: event.target.value,
-        });
-    };
+	// porque no enviarle el label y el event?
+	// const handleInputChange = (label, event) => {
 
-    const allValuesAreValid = Object.values(birthDate);
 
-    const handleAgeCalc = () => {
+  const handleInputChange = (label) => (event) => {
+		//No es necesario recuperar todo el brithDate
+    setBirthDate({
+      ...birthDate,
+      [label]: event.target.value,
+    });
+  };
 
-        if(allValuesAreValid.length === 3){
+  const allValuesAreValid = Object.values(birthDate);
 
-            let today = new Date();
-            let todayDay = today.getDate();
-            let todayMonth = today.getMonth() + 1;
-            let todayYear = today.getFullYear();
+  const handleAgeCalc = () => {
 
-            let daysOfMonth = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
+    if (allValuesAreValid.length === 3) {
 
-            if (birthDate.day > todayDay) {
-                todayDay = todayDay + daysOfMonth[todayMonth + 1];
-                todayMonth = todayMonth - 1;
-            }
+      let today = new Date();
+      let todayDay = today.getDate();
+      let todayMonth = today.getMonth() + 1;
+      let todayYear = today.getFullYear();
 
-            if (birthDate.month > todayMonth) {
-                todayMonth = todayMonth + 12;
-                todayYear = todayYear - 1;
-            }
+      let daysOfMonth = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
 
-            let years = parseInt(todayYear - birthDate.year);
-            let months = parseInt(todayMonth - birthDate.month);
-            let days = parseInt(todayDay - birthDate.day);
-   
-            setAge({...age, years: years, months: months, days: days})
+      if (birthDate.day > todayDay) {
+        todayDay = todayDay + daysOfMonth[todayMonth + 1];
+        todayMonth = todayMonth - 1;
+      }
+
+      if (birthDate.month > todayMonth) {
+        todayMonth = todayMonth + 12;
+        todayYear = todayYear - 1;
+      }
+
+      let years = parseInt(todayYear - birthDate.year);
+      let months = parseInt(todayMonth - birthDate.month);
+      let days = parseInt(todayDay - birthDate.day);
+
+			// No es necesario recuperar todo el objeto (...age, ...).
+      setAge({ ...age, years: years, months: months, days: days })
 
     } else {
-        console.error("Falta uno de los campos")
+      console.error("Falta uno de los campos")
     }
-    }
+  }
 
-        return (
-            <div className={styles.container}>
-                <div className={styles.container_div_inputs}>
-                    {
-                        data.map((elemento, item) => {
-                            return (
-                                <div key={item} className={styles.input}>
-                                    <label htmlFor={elemento.titulo} >{elemento.label}</label>
-                                    <input type='number' min="0" onChange={handleInputChange(elemento.label)}></input>
-                                </div>
-                            )
-                        })
-                    }
-                    <button className={styles.button} onClick={handleAgeCalc}>↓</button>
-                </div>
-                <div className={styles.container_items}>
+  return (
+    <div className={styles.container}>
+      <div className={styles.container_div_inputs}>
+        {
+          data.map((elemento, index) => {
+            return (
+              <div key={index} className={styles.input_container}>
+                <label htmlFor={elemento.titulo} >{elemento.label}</label>
+								{/* <input type='number' min="0" onChange={(event) => handleInputChange(elemento.label, event)}></input> */}
 
-                    <h1><span>{(age.days != 0) ? `${age.days}` : "--"}</span>days</h1>
-                    <h1><span>{(age.months != 0) ? `${age.months}` : "--"}</span>months</h1>
-                    <h1><span>{(age.years != 0) ? `${age.years}` : "--"}</span>years</h1>
+                <input type='number' min="0" onChange={handleInputChange(elemento.label)}></input>
+              </div>
+            )
+          })
+        }
+        <button className={styles.button} onClick={handleAgeCalc}>↓</button>
+      </div>
+      <div className={styles.container_items}>
 
-                </div>
-            </div>
-        )
+        <h1><span>{(age.days != 0) ? `${age.days}` : "--"}</span>days</h1>
+        <h1><span>{(age.months != 0) ? `${age.months}` : "--"}</span>months</h1>
+        <h1><span>{(age.years != 0) ? `${age.years}` : "--"}</span>years</h1>
+
+      </div>
+    </div>
+  )
 }
